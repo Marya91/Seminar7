44bf048ea738a0f04edbeab2b1128737ef6ac51b
Maria <newlalamhs@mail.ru>
2022-12-14 10:33:20 +0500

    task50

--- Program.cs
+++ Program.cs
@@ -1,34 +1,32 @@
-﻿/* Задача 47. Задайте двумерный массив размером m×n, 
-заполненный случайными вещественными числами.
-m = 3, n = 4.
-0,5 7 -2 -0,2
-1 -3,3 8 -9,9
-8 7,8 -7,1 9   */
+﻿/* Задача 50. Напишите программу, которая на вход принимает 
+позиции элемента в двумерном массиве, и возвращает значение 
+этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет  */
 
-Console.WriteLine("Задайте количество строк двумерного массива:");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Задайте количество столбцов двумерного массива:");
-int n = Convert.ToInt32(Console.ReadLine());
-double[,] twoDimArray = new double[m, n];
-Random rnd = new Random();
-void PrintArray(double[,] matr)
-{
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        { Console.Write($"{matr[i, j]} "); }
-        Console.WriteLine();
-    }
-}
+int[,] array = new int[3, 3];
 
-void FillArray(double[,] matr)
+for (int i = 0; i < array.GetLength(0); i++)
 {
-    for (int i = 0; i < m; i++)
+    for (int j = 0; j < array.GetLength(1); j++)
     {
-        for (int j = 0; j < n; j++)
-        { matr[i, j] = Convert.ToDouble(rnd.Next(-100, 100) / 10.0); }
+        array[i, j] = new Random().Next(10);
+        Console.Write($"{array[i, j]} ");
     }
+    Console.WriteLine();
 }
-FillArray(twoDimArray);
-Console.WriteLine();
-PrintArray(twoDimArray);
+int iu, ju;
+Console.Write($"Введите i: ");
+int.TryParse(Console.ReadLine(), out iu);
+Console.Write($"Введите j: ");
+int.TryParse(Console.ReadLine(), out ju);
+
+if (iu < 0 || iu >= array.GetLength(0))
+    Console.Write($"Неверный индекс строки");
+else if (ju < 0 || ju >= array.GetLength(1))
+    Console.Write($"Неверный индекс столбца");
+else
+    Console.Write($"[{iu},{ju}] = {array[iu, ju]}");
